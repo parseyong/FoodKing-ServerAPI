fbf6236153ae23d7c1ec23ad0c1f621b9b5f88f5
parseyong <psy217300@naver.com>
2023-10-31 00:02:16 +0900

    [Feat] - 초기 세팅(#4)

---
+++ src/test/java/com/example/foodking/FoodKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
